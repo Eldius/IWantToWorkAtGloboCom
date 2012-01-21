e9f9df89bae83b1ad95d5a791635a9755b0ec510
Eldius <eldiosantos@gmail.com>
2012-01-21 14:59:36 -0200

    Inclusão da classe de comunicação para nosso ar condicionado específico.
    Signed-off-by: Eldius <eldiosantos@gmail.com>

---
+++ src/main/java/net/eldiosantos/gobocom/arcondicionado/ArCondicionadoImpl.java
@@ -0,0 +1,32 @@
+package net.eldiosantos.gobocom.arcondicionado;
+
+import net.eldiosantos.gobocom.arcondicionado.interfaces.ArCondicionado;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Implementação para um ar condicionado específico.
+ * 
+ * @author Eldius
+ * 
+ */
+public class ArCondicionadoImpl implements ArCondicionado {
+
+	private static Logger logger = Logger.getLogger(ArCondicionadoImpl.class);
+
+	private boolean ligado = false;
+
+	public boolean isLigado() {
+		return this.ligado;
+	}
+
+	public void reduz_um_grau() {
+
+		StringBuffer sb = new StringBuffer("Status: ").append(ligado);
+
+		logger.info(sb.toString());
+
+		this.ligado = true;
+	}
+
+}
