f17b06c4df7da4ae878cecec274d56bcaa0131d4
Eldius <eldiosantos@gmail.com>
2012-01-21 15:42:06 -0200

    Inclusão da classe do controlador específico para nosso aparelho.
    Signed-off-by: Eldius <eldiosantos@gmail.com>

---
+++ src/main/java/net/eldiosantos/gobocom/arcondicionado/controller/DefaultController.java
@@ -0,0 +1,60 @@
+package net.eldiosantos.gobocom.arcondicionado.controller;
+
+import java.math.BigDecimal;
+
+import net.eldiosantos.gobocom.arcondicionado.interfaces.ArCondicionado;
+
+public class DefaultController implements Controller {
+
+	private ArCondicionado arCondicionado;
+	private BigDecimal valor = new BigDecimal(0.00);
+
+	private float tempAtual;
+
+	public DefaultController(ArCondicionado arCondicionado) {
+		super();
+		this.arCondicionado = arCondicionado;
+	}
+
+	public BigDecimal costoTotal() {
+		return this.valor;
+	}
+
+	public BigDecimal manterTemperatura(float tempInicial, float tempFinal,
+			int minutos) {
+
+		this.tempAtual = tempInicial;
+		BigDecimal valorAtual = new BigDecimal(0.00);
+		float margem = tempFinal + 2;
+
+		while (tempAtual > tempFinal) {
+			valorAtual.add(this.baixarTemperatura());
+		}
+
+		for (int t = 0; t < minutos; t++) {
+			tempAtual += 0.5;
+
+			if (!(margem < tempAtual)) {
+				valorAtual.add(this.baixarTemperatura());
+			}
+		}
+
+		this.valor.add(valorAtual);
+
+		return valorAtual;
+	}
+
+	private BigDecimal baixarTemperatura() {
+		BigDecimal resposta = new BigDecimal(0.00);
+
+		if (arCondicionado.isLigado()) {
+			resposta.add(new BigDecimal(0.10));
+		} else {
+			resposta.add(new BigDecimal(0.50));
+		}
+
+		arCondicionado.reduz_um_grau();
+
+		return resposta;
+	}
+}
