93e607720555f2cc523a51bf1f41041908483737
Eldius <eldiosantos@gmail.com>
2012-01-21 14:55:01 -0200

    Inclusão de comentários Javadoc na interface.
    Signed-off-by: Eldius <eldiosantos@gmail.com>

--- src/main/java/net/eldiosantos/gobocom/arcondicionado/interfaces/ArCondicionado.java
+++ src/main/java/net/eldiosantos/gobocom/arcondicionado/interfaces/ArCondicionado.java
@@ -1,8 +1,22 @@
 package net.eldiosantos.gobocom.arcondicionado.interfaces;
 
+/**
+ * Interface que define a comunicação com o aparelho.
+ * 
+ * @author Eldius
+ * 
+ */
 public interface ArCondicionado {
 
+	/**
+	 * Verifica status do aparelho.
+	 * 
+	 * @return status do aparelho.
+	 */
 	public abstract boolean isLigado();
 
+	/**
+	 * Reduz em um grau a temperatura ambiente.
+	 */
 	public abstract void reduz_um_grau();
 }
