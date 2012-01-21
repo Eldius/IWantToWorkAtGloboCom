14af631328caa13bd78efcb48f5eaf892c6ba1a2
Eldius <eldiosantos@gmail.com>
2012-01-21 16:34:55 -0200

    Correção no uso da classe BigDecimal.
    Signed-off-by: Eldius <eldiosantos@gmail.com>

--- src/main/java/net/eldiosantos/gobocom/arcondicionado/controller/DefaultController.java
+++ src/main/java/net/eldiosantos/gobocom/arcondicionado/controller/DefaultController.java
@@ -2,9 +2,14 @@ package net.eldiosantos.gobocom.arcondicionado.controller;
 
 import java.math.BigDecimal;
 
+import net.eldiosantos.gobocom.arcondicionado.controller.interfaces.Controller;
 import net.eldiosantos.gobocom.arcondicionado.interfaces.ArCondicionado;
 
+import org.apache.log4j.Logger;
+
 public class DefaultController implements Controller {
+
+	private static Logger logger = Logger.getLogger(DefaultController.class);
 
 	private ArCondicionado arCondicionado;
 	private BigDecimal valor = new BigDecimal(0.00);
@@ -14,6 +19,7 @@ public class DefaultController implements Controller {
 	public DefaultController(ArCondicionado arCondicionado) {
 		super();
 		this.arCondicionado = arCondicionado;
+		valor.setScale(2);
 	}
 
 	public BigDecimal costoTotal() {
@@ -27,9 +33,11 @@ public class DefaultController implements Controller {
 		BigDecimal valorAtual = new BigDecimal(0.00);
 		float margem = tempFinal + 2;
 
+		valorAtual.setScale(2);
+
 		// Baixar temperatura para a desejada.
 		while (tempAtual > tempFinal) {
-			valorAtual.add(this.baixarTemperatura());
+			valorAtual = valorAtual.add(this.baixarTemperatura());
 		}
 
 		// Manter a temperatura dentro da margem aceitável pelo tempo
@@ -40,27 +48,33 @@ public class DefaultController implements Controller {
 			// Se a temperatura não estiver abaixo da margem baixar a
 			// temperatura.
 			if (!(tempAtual < margem)) {
-				valorAtual.add(this.baixarTemperatura());
+				valorAtual = valorAtual.add(this.baixarTemperatura());
 			}
+
+			logger.debug("Valor atual: " + valorAtual);
 		}
 
-		this.valor.add(valorAtual);
+		this.valor = this.valor.add(valorAtual);
 
 		return valorAtual;
 	}
 
 	private BigDecimal baixarTemperatura() {
 		BigDecimal resposta = new BigDecimal(0.00);
+		resposta.setScale(2);
 
 		if (arCondicionado.isLigado()) {
-			resposta.add(new BigDecimal(0.10));
+			resposta = resposta.add(new BigDecimal("0.10"));
 		} else {
-			resposta.add(new BigDecimal(0.50));
+			resposta = resposta.add(new BigDecimal("0.50"));
 		}
 
 		arCondicionado.reduz_um_grau();
 
 		this.tempAtual--;
+
+		logger.debug("Temperatura: " + tempAtual);
+		logger.debug("Custo: " + resposta);
 
 		return resposta;
 	}
